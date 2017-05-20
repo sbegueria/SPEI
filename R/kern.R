8ef47c5865aab1507935a1ab5431a1b55e94542e
Doug Friedman <sillyville+github@gmail.com>
2017-05-20 11:18:13 -0400

    Refactoring kern with switch

--- R/kern.R
+++ R/kern.R
@@ -93,12 +93,17 @@ kern <- function(scale, type='rectangular', shift=0) {
 	if(s<=0) {
 		stop('Parameter "scale" must be higher than zero')
 	}
-	#
-	if(type=='rectangular' | s==1) k <- rep(1,s)
-	if(type=='triangular') k <- s:1
-	if(type=='circular') k <- (s^2+(1-(1:s)^2))
-	if(type=='gaussian') k <- (1/0.4)*1/sqrt(2*pi*1^2)*
-		exp(-(seq(0,-3,-3/(s-1))-0)^2/2*1^2)
+	
+	if(s == 1) type == "rectangular"
+	
+	k = switch(type,
+	       rectangular = rep(1,s),
+	       triangular = s:1,
+	       circular = (s^2+(1-(1:s)^2)),
+	       gaussian = (1/0.4)*1/sqrt(2*pi*1^2)*
+	                  exp(-(seq(0,-3,-3/(s-1))-0)^2/2*1^2)
+	    )
+
 	if(h) k <- c(k[(h+1):2],k[1:(s-h)])
 	#return(k/sum(k))
 	return(k/mean(k))
