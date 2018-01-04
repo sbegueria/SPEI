1c019b24660a8b192dc608b35541b8fb06714db6
santiago <santiago.begueria@csic.es>
2018-01-04 16:29:52 +0100

    Adding SPEI-package.r

---
+++ R/SPEI-package.r
@@ -0,0 +1,43 @@
+#' SPEI.
+#'
+#'
+#' @name SPEI
+#' @docType package
+#' 
+#'
+#' @description  
+#' A set of functions for computing potential evapotranspiration and several widely 
+#' used drought indices including the Standardized Precipitation-Evapotranspiration 
+#' Index (SPEI).
+#'
+#' @details
+#' \tabular{ll}{
+#'   Package: \tab SPEI\cr
+#'   Type: \tab Package\cr
+#'   Version: \tab 1.4\cr
+#'   Date: \tab 2011-26-09\cr
+#'   License: \tab GPL version 2 or newer\cr
+#'   LazyLoad: \tab yes\cr
+#' }
+#'   
+#' Functions \code{\link{spei}} and \code{\link{spi}} are the workhorse of the 
+#' SPEI library. Other functions such as \code{\link{kern}}, \code{\link{parglo.maxlik}} 
+#' is an auxiliary low-level function and will not be used 
+#' directly by the typical user. Functions for computing potential evapotranspiration 
+#' are provided, too, for helping computing the SPEI. They are: \code{\link{thornthwaite}}, 
+#' \code{\link{hargreaves}} and \code{\link{penman}}.
+#'      
+#' @author     
+#' Santiago Beguería and Sergio M. Vicente-Serrano
+#' Maintainer: Santiago Beguería
+#' 
+#' 
+#' @references 
+#' S.M. Vicente-Serrano, S. Beguería, J.I. López-Moreno. 2010. A Multi-scalar drought 
+#' index sensitive to global warming: The Standardized Precipitation Evapotranspiration 
+#' Index – SPEI. \emph{Journal of Climate} \bold{23}: 1696, DOI: 10.1175/2009JCLI2909.1.
+#' 
+#' \url{http://sac.csic.es/spei/}
+#' @keywords package
+#' 
+NULL
