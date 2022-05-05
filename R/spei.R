27a00e867a92bc368057aca731a0638b10b5bd1e
Santiago Beguería <santiago.begueria@csic.es>
2022-05-05 17:56:09 +0200

    plot.spei(): updated function
    The plot.spei() method has been rewritten from scratch using ggplot2.

--- R/spei.R
+++ R/spei.R
@@ -753,51 +753,123 @@ summary.spei <- function (object, ...) {
 #' 
 #' @export
 #' 
-plot.spei <- function (x, ttext=NULL, ...) {
-  label <- ifelse(as.character(x$call)[1]=='spei','SPEI','SPI')
-	ser <- ts(as.matrix(x$fitted[-c(1:x$scale),]),
-		end=end(x$fitted),frequency=frequency(x$fitted))
-	ser[is.nan(ser-ser)] <- 0
-	se <- ifelse(ser==0,ser,NA)
-	if(is.null(ttext)){
-	  tit <- paste(label, dimnames(x$coefficients)[2][[1]])
-	} else {
-	  tit = paste(label, ttext, seq_along(dim(x$coefficients)[2]))
-	}
-	#
-	if (start(ser)[2]==1) {
-		ns <- c(start(ser)[1]-1,12)
-	} else {
-		ns <- c(start(ser)[1],start(ser)[2]-1)	
-	}
-	if (end(ser)[2]==12) {
-		ne <- c(end(ser)[1]+1,1)
-	} else {
-		ne <- c(end(ser)[1],end(ser)[2]+1)
-	}
-	#
-	n <- ncol(ser)
-	if (is.null(n)) n <- 1
-	par(mar=c(4,4,2,1)+0.1)
-	if (n>1 & n<5) par(mfrow=c(n,1))
-	if (n>1 & n>=5) par(mfrow=c({n+1}%/%2,2))
-	for (i in 1:n) {
-		datt <- ts(c(0,ser[,i],0),frequency=frequency(ser),start=ns,end=ne)
-		datt.pos <- ifelse(datt>0,datt,0)
-		datt.neg <- ifelse(datt<=0,datt,0)
-		plot(datt, type='n', xlab='', ylab=paste(label, "(z-values)"),main=tit[i])
-		if (!is.null(x$ref.period)) {
-			k <- ts(5,start=x$ref.period[1,],end=x$ref.period[2,],frequency=12)
-			k[1] <- k[length(k)] <- -5
-			polygon(k, col='light grey',border=NA,density=20)
- 			abline(v=x$ref.period[1,1]+(x$ref.period[1,2]-1)/12,col='grey')
-			abline(v=x$ref.period[2,1]+(x$ref.period[2,2]-1)/12,col='grey')
-		}
-		grid(col='black')
-		polygon(datt.pos,col='blue',border=NA)
-		polygon(datt.neg,col='red',border=NA)
-		lines(datt,col='dark grey')
-		abline(h=0)
-		points(se,pch=21,col='white',bg='black')
-	}
+plot.spei <- function (x) {
+  
+  ### Argument check - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  
+  if (class(x) != 'spei') {
+    stop('Data must be an `spei` object resulting from a call to spi() or spei() functions.')
+  }
+  
+  
+  ### Make the plot - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  
+  # Label
+  if (grepl('spei', x$call[1])) {
+    label <- 'SPEI (z-values)'
+  } else if (grepl('spi', x$call[1])) {
+    label <- 'SPI (z-values)'
+  } else {
+    stop('Label could not be determined. Please, report the error.')
+  }
+  
+  # Extract the data
+  data <- x$fitted
+  
+  # Determine input data shape
+  if (is.ts(data)) {
+    if (is.matrix(data)) {
+      out_type <- 'tsmatrix'
+    } else {
+      out_type <- 'tsvector'
+    }
+  } else if (is.vector(data)) {
+    out_type <- 'vector'
+  } else if (is.matrix(data)) {
+    out_type <- 'matrix'
+  } else if (is.array(data)) {
+    out_type <- 'array'
+  }
+  
+  # Determine time properties
+  if (is.ts(data)) {
+    ts_freq <- frequency(data)
+    ts_start <- start(data)
+  } else {
+    ts_freq <- 12
+    ts_start <- 1
+  }
+  
+  # Create uniformly-dimensioned ts-matrices from input for internal use (acu)
+  if (out_type == 'vector' | out_type == 'tsvector') {
+    data <- as.matrix(data)
+  } else if (out_type == 'matrix' | out_type == 'tsmatrix') {
+    data <- data
+  } else if (out_type == 'array') {
+    data <- matrix(data, ncol=dim(data)[2] * dim(data)[3])
+  }
+  
+  # Check on the dimensions; default max. is 10 sites
+  if (ncol(data) > 10) {
+    data <- data[,1:10]
+    warning('Maximum allowed sites is ten. Plotting the first ten sites in the data.')
+  }
+  
+  # Convert to time series
+  if (!is.ts(data)) {
+    data <- ts(data, start=ts_start, fr=ts_freq)
+  }
+  
+  # Determine reference period
+  if (!is.null(x$ref.period)) {
+    if (grepl('ref.start', paste(row.names(x$ref.period), collapse=' '))) {
+      ref1 <- x$ref.period['ref.start',1] + (x$ref.period['ref.start',2]-0.5) / 12
+    } else {
+      ref1 <- start(data)[1] + (start(data)[2]-0.5) / 12
+    }
+    if (grepl('ref.end', paste(row.names(x$ref.period), collapse=' '))) {
+      ref2 <- x$ref.period['ref.end',1] + (x$ref.period['ref.end',2]-0.5) / 12
+    } else {
+      ref2 <- end(data)[1] + (end(data)[2]-0.5) / 12
+    }
+  } else {
+    ref1 <- ref2 <- NULL
+  }
+  
+  # Melt
+  kk <- as.data.frame(data)
+  kk$time <- as.character(time(data))
+  kk <- reshape::melt(kk, id.vars='time')
+  kk$time <- as.numeric(kk$time)
+  kk <- kk[!is.na(kk$value),]
+  
+  # Add SPI / SPEI categories
+  kk$cat <- ifelse(kk$value > 0, 'neg', 'pos')
+  
+  # Plot it
+  g <- ggplot(kk, aes(time, value, fill=cat, color=cat))
+  # reference period (if different than whole series)
+  if (!is.null(x$ref.period)) {
+    g <- g +
+      annotate('rect', xmin=ref1, xmax=ref2, ymin=-Inf, ymax=Inf, alpha=0.2) +
+      geom_vline(xintercept=c(ref1,ref2), color='grey', alpha=0.4)
+  }
+  # add the bars with the SPEI values
+  g <- g +
+    geom_bar(stat='identity') +
+    scale_fill_manual(values=c('cyan3','tomato')) +  # new look
+    scale_color_manual(values=c('cyan3','tomato')) # new look
+  # add other parts and options
+  g <- g +
+    geom_hline(yintercept=0, color='grey') +
+    facet_wrap(~variable, ncol=1) +
+    scale_y_continuous(breaks=seq(-2, 2, 0.5)) +
+    ylab(label) +
+    xlab('Time') +
+    theme_bw() +
+    theme(legend.position='none',
+          strip.background=element_blank(),
+          strip.text=element_text(hjust=0))
+  
+  return(g)
 }
